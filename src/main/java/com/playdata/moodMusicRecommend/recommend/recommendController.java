e5d697b8350d107b73e0b16c46b94cd38ae0d1b7
JooyeopHan <85021904+JooyeopHan@users.noreply.github.com>
2023-02-17 11:09:52 +0900

    fix:file

--- src/main/java/com/playdata/moodMusicRecommend/recommend/recommendController.java
+++ src/main/java/com/playdata/moodMusicRecommend/recommend/recommendController.java
@@ -93,7 +93,7 @@ public class RecommendController {
 
         } catch (UnsupportedEncodingException e)
         {
-            
+
             e.printStackTrace();
         } catch (IllegalStateException e) {
             e.printStackTrace();
