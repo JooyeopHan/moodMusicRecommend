e7e86937a721ee7e3055efa2cac9db9c9d05a690
dayflys <kkwr0504@naver.com>
2023-02-21 16:49:03 +0900

    feedback 완료

--- DataAnaylsis/vaplane.py
+++ DataAnaylsis/vaplane.py
@@ -5,3 +5,9 @@ def MakePlane():
     museProcess.process(muse)
     result = museProcess.VAPlane(muse)
     return result
+
+# 평면 -> 곡 매핑 -> 사용자 감정 벡터 생산 -> 거리 기반 유사한
+# 사용자 감정 벡터 생산 -> 거리 기반 유사한 => model 작업
+# 감정 인지 모델 제작
+#
+# luxand face api
