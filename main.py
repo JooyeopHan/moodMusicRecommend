cc784bcfad52ec0655a9c32eaf9b4a485bd1ef4d
dayflys <kkwr0504@naver.com>
2023-02-21 12:30:08 +0900

    feedback 완료

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pprint
 
 from flask import Flask,request
 import EmotionRecognition
@@ -14,6 +15,10 @@ def detecting():
     #이미지 파일 받고 저장
     before_data = request.form['file1']
     after_data = request.form['file2']
+    va = eval(request.form['member'])
+    plane = {'anger': [va['angerV'],va['angerA']],'happiness': [va['happinessV'],va['happinessA']],'sadness': [va['sadnessV'],va['sadnessA']],'surprise': [va['surpriseV'],va['surpriseA']],'disgust': [va['disgustV'],va['disgustA']],'fear': [va['fearV'],va['fearA']],'neutral': [va['neutralV'],va['neutralA']]}
+    pprint.pprint(plane)
+    #va = request.args.get('vaplane')
     # before_data = io.BytesIO(before_data)
     # before_data2 = base64.b64decode(before_data)
     print(1)
@@ -27,16 +32,25 @@ def detecting():
 
     file = [before_data,after_data]
     # print(file)
+
+
     client = EmotionRecognition.initial()
-    emotions_position = vaplane.MakePlane()
+    # emotions_position = vaplane.MakePlane()
+    # print(emotions_position)
+
+
     # # 전/ 후 사진으로 추출된 벡터
-    result = EmotionRecognition.emotioning(file,client,emotions_position)
+    result = EmotionRecognition.emotioning(file,client,plane)
     # print(result)
     sp = Spotify.spotify.initial()
     pre_tracklist = Spotify.spotify.spotifing(sp)
+
+
     # print(pre_tracklist, type(pre_tracklist))
     # # 랜덤 추출된 50개의 곡 dataframe, column = ['artist_name','track_name','energy', 'valence']
     # print(pre_tracklist)
+
+
     resulted = sortingInterface.sorting(pre_tracklist,result[0])
     end = {'list': resulted, 'result':[result[1],result[2]]}
     #result = 10개의 중복되지 않은 [[artist,name],...]
