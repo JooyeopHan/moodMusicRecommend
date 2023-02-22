149106e541e8176fc2072d9770cfc2f8178ead20
JooyeopHan <jooyeop96@gamail.com>
2023-02-22 14:06:58 +0900

    done: 사용자 추천 구현 완료

--- src/main/java/com/playdata/moodMusicRecommend/member/MemberService.java
+++ src/main/java/com/playdata/moodMusicRecommend/member/MemberService.java
@@ -1,20 +1,19 @@
 package com.playdata.moodMusicRecommend.member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
 
-    private final MemberRepository repository;
+    private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
     public Member create(String nickname, String email, String passwd){
@@ -24,18 +23,130 @@ public class MemberService {
 
         member.setPasswd(passwordEncoder.encode(passwd)); //passwd 암호화
 
-        this.repository.save(member);
+        this.memberRepository.save(member);
 
         return member;
     }
 
     public Optional<Member> select(String nickname){
-        Optional<Member> member =  repository.findByNickname(nickname);
+        Optional<Member> member =  memberRepository.findByNickname(nickname);
 
         return member;
     }
 
     public void delete(User user) {
-        repository.deleteByNickname(user.getUsername());}
+        memberRepository.deleteByNickname(user.getUsername());}
+
+    // 변경 사항 저장
+    public void update(Member member){
+        this.memberRepository.save(member);
+    }
+
+    public boolean testing(String test,User user){
+
+        Member presentUser;
+        //post JSON.stringfy -> string { 'arousal': } -> '{ \'arousal\' : } <- string
+        // request parsing
+        JSONObject job = new JSONObject(test);
+        JSONArray list = job.getJSONArray("list");
+        JSONArray result = job.getJSONArray("result");
+        String emotion = result.getString(1);
+        String pre_emotion = result.getString(0);
+        String nickname = job.getString("nickname");
+        Double[] means = new Double[2];
+        double result_valence = 0;
+        double pre_valence = 0;
+        double result_arousal = 0;
+        double pre_arousal = 0;
+
+        // DB에서 user 감정 셋을 끌어옴
+        Optional<Member> mem = memberRepository.findByNickname(user.getUsername());
+        if(mem.isPresent()){
+            System.out.println(mem.get().getHappinessV());
+            switch (pre_emotion){
+                case "anger":
+                    pre_valence += mem.get().getAngerV();
+                    pre_arousal += mem.get().getAngerA();
+                    break;
+                case "happiness":
+                    pre_valence += mem.get().getHappinessV();
+                    pre_arousal += mem.get().getHappinessA();
+                    break;
+                case "disgust":
+                    pre_valence += mem.get().getDisgustV();
+                    pre_arousal += mem.get().getDisgustA();
+                    break;
+                case "fear":
+                    pre_valence += mem.get().getFearV();
+                    pre_arousal += mem.get().getFearA();
+                    break;
+                case "neutral":
+                    pre_valence += mem.get().getNeutralV();
+                    pre_arousal += mem.get().getNeutralA();
+                    break;
+                case "sadness":
+                    pre_valence += mem.get().getSadnessV();
+                    pre_arousal += mem.get().getSadnessA();
+                    break;
+                case "surprise":
+                    pre_valence += mem.get().getSurpriseV();
+                    pre_arousal += mem.get().getSurpriseA();
+                    break;
+            }
+        }
+
+        System.out.println(list);
+        System.out.println(result);
+        System.out.println(nickname);
+        for(int i = 0; i<list.length();i++){
+            JSONObject obj = list.getJSONObject(i);
+            double valence = obj.getDouble("valence");
+            double arousal = obj.getDouble("arousal");
+            result_valence += valence;
+            result_arousal += arousal;
+        }
+        result_valence /= list.length();
+        result_arousal /= list.length();
+
+        //happiness 일때 좋아요를 누른 곡의 평균
+        means[0] = Math.round(result_valence*10000)/10000.0;
+        means[1] = Math.round(result_arousal*10000)/10000.0;
+        System.out.println(means[0]);
+        System.out.println(means[1]);
+        // DB 에 보낸다.
+        presentUser = mem.get();
+        switch (emotion){
+            case "anger":
+                presentUser.setAngerV(means[0]+pre_valence);
+                presentUser.setAngerA(means[1]+pre_arousal);
+                break;
+            case "happiness":
+                presentUser.setHappinessV(means[0]+pre_valence);
+                presentUser.setHappinessA(means[1]+pre_arousal);
+                break;
+            case "disgust":
+                presentUser.setDisgustV(means[0]+pre_valence);
+                presentUser.setDisgustA(means[1]+pre_arousal);
+                break;
+            case "fear":
+                presentUser.setFearV(means[0]+pre_valence);
+                presentUser.setFearA(means[1]+pre_arousal);
+                break;
+            case "neutral":
+                presentUser.setNeutralV(means[0]+pre_valence);
+                presentUser.setNeutralA(means[1]+pre_arousal);
+                break;
+            case "sadness":
+                presentUser.setSadnessV(means[0]+pre_valence);
+                presentUser.setSadnessA(means[1]+pre_arousal);
+                break;
+            case "surprise":
+                presentUser.setSurpriseV(means[0]+pre_valence);
+                presentUser.setSurpriseA(means[1]+pre_arousal);
+                break;
+        }
+        this.update(presentUser);
+        return true;
+    }
 
 }
--- src/main/java/com/playdata/moodMusicRecommend/recommend/RecommendController.java
+++ src/main/java/com/playdata/moodMusicRecommend/recommend/RecommendController.java
@@ -1,8 +1,16 @@
 package com.playdata.moodMusicRecommend.recommend;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.playdata.moodMusicRecommend.ResultDTO;
+import com.playdata.moodMusicRecommend.member.Member;
+import com.playdata.moodMusicRecommend.member.MemberRepository;
+import com.playdata.moodMusicRecommend.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -13,11 +21,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @Controller
@@ -25,11 +29,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class RecommendController {
 
-    private final RecommendService service;
+    private final RecommendService recommendService;
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
     @PostMapping("/music")
     @ResponseBody
-    public String music(MultipartHttpServletRequest mhsr) throws IOException {
-        MultiValueMap<String, String> builder = new LinkedMultiValueMap<>();
+    public String music(MultipartHttpServletRequest mhsr ,@AuthenticationPrincipal User user) throws IOException {
+        MultiValueMap<String, Object> builder = new LinkedMultiValueMap<>();
 // decode base64 encoded image data
         String file1Data = mhsr.getParameter("file1").substring(22);
         String file2Data = mhsr.getParameter("file2").substring(22);
@@ -37,6 +43,8 @@ public class RecommendController {
 // create multipart body parts and add them to the builder
         builder.add("file1", file1Data);
         builder.add("file2", file2Data);
+        Optional<Member> mem = memberRepository.findByNickname(user.getUsername());
+        mem.ifPresent(member ->  builder.add("member", member));
 
         WebClient client = WebClient.create();
 
@@ -51,24 +59,32 @@ public class RecommendController {
 
     @PostMapping("/list")
     @ResponseBody
-    public String Like(@RequestBody Map<String, Object> test, @AuthenticationPrincipal User user) {
+    public ResponseEntity<ResultDTO> Like(@RequestBody Map<String, Object> test, @AuthenticationPrincipal User user) throws JsonProcessingException {
         System.out.println("recommend/list 접속");
-
+        ResultDTO dto = new ResultDTO();
+        HttpHeaders headers = new HttpHeaders();
+        dto.setMsg("성공적으로 저장되었습니다.");
+        dto.setRes(false);
+        dto.setUrl("/emotion");
 
         // 데이터 저장
 
-        service.create(test);
+        recommendService.create(test);
 
         System.out.println("성공?");
-        // 저장된 데이터 추출
 
         // 추출된 데이터 기반 갱신
+        ObjectMapper mapper = new ObjectMapper();
+        String testing = mapper.writeValueAsString(test);
+        String testing1 = test.toString();
+        System.out.println(testing1);
+        System.out.println(testing);
+        boolean result = memberService.testing(testing, user);
 
         // 갱신된 결과 User 저장
 
-        return "hello";
-
+        dto.setRes(result);
+        return ResponseEntity.accepted().headers(headers).body(dto);
     }
-
 
 }
